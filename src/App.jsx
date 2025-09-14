2bea7a279af4270e0a64bc207ffdfa988da262be
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-14 16:52:43 +0530

    finished the basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 // import "./App.css";
 
-import { Link, Outlet, Route,Routes } from "react-router";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Store from "./Components/Store";
 import About from "./Components/About";
 
 function App() {
-
   return (
     <Routes>
-      <Route path='/' element={<NavWrapper/>} >
-      <Route path='/' element={<Home/>} />
-      <Route path='/store' element={<Store/>} />
-      <Route path='/about' element={<About/>} />
+      <Route path="/" element={<NavWrapper />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
   );
 }
 
-
-function NavWrapper(){
+function NavWrapper() {
   return (
     <>
-    <nav>
-      <Link to="/">Home....</Link>
-      <Link to="/store">Store</Link>
-      <Link to="/about">About</Link>
-    </nav>
-    <Outlet/>
+      <nav style={{display:"flex",gap:"1rem"}}>
+        <Link to="/">Home....</Link>
+        <Link to="/store">Store</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Outlet />
     </>
-  )
+  );
 }
 
 export default App;
