6ca20cc4af4a521240af342f9f6e7cff4d4678a1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-14 17:16:51 +0530

    basic setup finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-// import "./App.css";
-
-import { Link, Outlet, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import Store from "./Components/Store";
+import Home from "./Components/Home";
 import About from "./Components/About";
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<NavWrapper />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Route>
-    </Routes>
+    <>
+      <h2>Hello</h2>
+      <Routes>
+        <Route path="/" element={<NavWrapper />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
 function NavWrapper() {
   return (
     <>
-      <nav style={{display:"flex",gap:"1rem"}}>
-        <Link to="/">Home....</Link>
+      <nav style={{ display: "flex", gap: "1rem", fontSize: "2rem" }}>
+        <Link to="/">Home</Link>
         <Link to="/store">Store</Link>
         <Link to="/about">About</Link>
       </nav>
-      <Outlet />
     </>
   );
 }
-
-export default App;
--- src/Components/Store.jsx
+++ src/Components/Store.jsx
@@ -5,7 +5,7 @@ export default function Store() {
 
     return (
         <>
-           <h2>Store</h2> 
+          <h2>Store</h2>  
         </>
     )
 }
