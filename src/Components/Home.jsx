97050ebf66ca34a045cfc5988d64031f2887d9be
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-15 17:43:11 +0530

    made a few canges to import

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import AdminData from "./AdminData";
+// import Sum from "../sum";
 
 export default function Home() {
   const [isAdmin, setIsAdmin] = useState(false);
 
-  const sum = (a, b) => {
-    return a + b;
-  };
-
   return (
     <>
       <h1>Home</h1>
-      <button onClick={() => alert(sum(2, 2))}>2+2</button>
+      <button
+        onClick={() => {
+          import("../sum").then((module) => {
+            alert(module.default(4,3));
+          });
+        }}
+      >
+        4+3
+      </button>
       <br></br>
       <br></br>
       <br></br>
-      <button onClick={() => setIsAdmin(prev => !prev)}>Toggle Admin</button>
-      {isAdmin ? <AdminData/> : <h2>Not Admin</h2>}
+      <button onClick={() => setIsAdmin((prev) => !prev)}>Toggle Admin</button>
+      {isAdmin ? <AdminData /> : <h2>Not Admin</h2>}
     </>
   );
 }
