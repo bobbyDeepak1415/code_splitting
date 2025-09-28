1501f3b9c1f840c1d74dfc72c5884c43c59709fd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-29 01:25:46 +0530

    set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import FileUploader from "./components/FileUploader"
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import About from "./components/About";
 
 function App() {
-
   return (
     <div>
-      <FileUploader/>
+      <Routes>
+        <Route path="/" element={<NavWrapper />}>
+          <Route path="/" element={<Home />}>
+            Home
+          </Route>
+          <Route path="/about" element={<About />}>
+            About
+          </Route>
+          <Route path="/store" element={<Store />}>
+            Store
+          </Route>
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function NavWrapper() {
+  return (
+    <div>
+      <nav style={{display:"flex",gap:"1rem",fontSize:"30px"}}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+
+        <Link to="/store">store</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
