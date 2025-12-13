a4fda55c861b79adeaf93ffe82de34e1ccf96719
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-13 16:13:56 +0530

    final changes made

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Outlet, Route, Routes } from "react-router-dom";
 import Home1 from "./Home1";
+import About1 from "./About1";
+import Store1 from "./Store1";
 
 const App1 = () => {
   return (
