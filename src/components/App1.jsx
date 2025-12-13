55daf24e44e811c4dd4665bbf2972a197124e491
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-13 16:12:46 +0530

    final changes made

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+import Home1 from "./Home1";
 
 const App1 = () => {
   return (
     <div>
-      <h1>HAELLO</h1>
+      <Routes>
+        <Route path="/" element={<NavWrapper />}>
+          <Route path="/" element={<Home1 />}>
+            Home1
+          </Route>
+          <Route path="/about1" element={<About1 />}>
+            About1
+          </Route>
+          <Route path="/store1" element={<Store1 />}>
+            Store1
+          </Route>
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App1
+export default App1;
+
+function NavWrapper() {
+  return (
+    <>
+      <div>
+        <nav>
+          <Link to="/">Home1</Link>
+          <Link to="/about1">about1</Link>
+          <Link to="/store1">Store1</Link>
+        </nav>
+
+        <Outlet />
+      </div>
+    </>
+  );
+}
