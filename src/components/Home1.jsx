3170922e7a9733a9a9a3884064dbd31be8d85b92
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-14 19:33:05 +0530

    final changes made

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import sum from "../sum";
 
 const Home1 = () => {
   return (
     <div>
       <h1>Home1</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button onClick={() => alert(sum(2, 9))}>Add 2,9</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home1
+export default Home1;
