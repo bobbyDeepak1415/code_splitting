ba715b5f725305351ac0e7e1db6e4f930270de11
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-09 15:44:42 +0530

    setup done

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,67 +1,35 @@
-import React, { Suspense } from "react";
-import { Link, Outlet, Route, Routes } from "react-router-dom";
-// import { Store1 } from "./Store1";
-// import Home1 from "./Home1";
-// import About1 from "./About1";
-
-const Home1 = React.lazy(() =>
-  import("./Home1").then((module) => {
-    return {
-      default: module.default,
-    };
-  }),
-);
-
-const About1 = React.lazy(() =>
-  import("./About1").then((module) => {
-    return {
-      default: module.default,
-    };
-  }),
-);
-
-const Store1 = React.lazy(() =>
-  import("./Store1").then((module) => {
-    return {
-      default: module.Store1,
-    };
-  }),
-);
+import React from 'react'
+import { Link, Routes } from 'react-router-dom'
+import Home1 from './Home1'
+import About1 from './About1'
+import { Store1 } from './Store1'
 
 const App1 = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<NavWrapper />}>
-          <Route path="/" element={<Home1 />}>
-            Home
-          </Route>
-          <Route path="/about" element={<About1 />}>
-            About
-          </Route>
-          <Route path="store" element={<Store1 />}>
-            Store
-          </Route>
+        <Route path='/' element={<NavWrapper/>}>
+        <Route path='/' element={<Home1/>}>Home</Route>
+        <Route path='/' element={<About1/>}>About</Route>
+        <Route path='/' element={<Store1/>}>Store</Route>
+
         </Route>
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App1;
+export default App1
 
-function NavWrapper() {
+
+function NavWrapper(){
+
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/store">Store</Link>
-      </nav>
-
-      <Suspense fallback={<h2>Loading...</h2>}>
-        <Outlet />
-      </Suspense>
+    <nav>
+      <Link></Link>
+    </nav>
     </>
-  );
+  )
+
 }
--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,21 +1,11 @@
-// import { sum } from "../sum";
-
-// import { sum } from "../sum";
+import React from 'react'
 
 const Home1 = () => {
   return (
     <div>
-      <h1>Home1</h1>
-
-      <button
-        onClick={() =>
-          import("../sum").then((module) => console.log(module.sum(3, 4)))
-        }
-      >
-        Add 3,4
-      </button>
+      <h2>Home111</h2>
     </div>
-  );
-};
+  )
+}
 
-export default Home1;
+export default Home1
